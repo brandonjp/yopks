d34167cb304c69dc6038ba045f89c36eeef1dbfb
brandonjp <brandonjp@gmail.com>
2011-02-14 13:50:40 -0500

    beginning to style

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 jQuery(document).ready(function($) {
 
-	$('div#addNewSelector ul li.modKeys ul li').click(function() {
-		var thisImgAlt = $(this).find('img').attr('alt');
-		$('div#addNewSelector ul li.textVersion input').hide().val(thisImgAlt);
-	});
+	var textVersionValue = '';
+
+	$('div#addNewSelector').find('ul.thisIsOneEntry li.modKeys ul li').click(function() {
+
+		// toggle the class & set a var for the alt value
+		$(this).toggleClass('selected');
+	
+		print2textVersion();
+
+	}); // end click
+	
+	function print2textVersion() {
+
+		var modKeysText = '';
+
+		$('li.modKeys ul li.selected').each(function() {
+			var thisModKeyAlt = $(this).find('img').attr('alt') + ' ';
+			modKeysText = modKeysText + thisModKeyAlt;
+		});
+
+		var extraKeysText = $('li.extraKeys').children('input').val();
+
+		$('li.textVersion input').val(modKeysText + ' ' + extraKeysText);
+
+	}
 
 });
